3627da8249ea47dc7f7fc76d7ff2abcc17c67435
Ivan <grabovivan@gmail.com>
2023-02-13 21:00:00 +0300

    input method added

--- hw1/num1.java
+++ hw1/num1.java
@@ -1,7 +1,17 @@
 package hw1;
 
+import java.util.Scanner;
+
 public class num1 {
     public static void main(String[] args) {
         
+    }
+
+    public static int input(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите N(порядок треугольного числа): ");
+        int line = scanner.nextInt();
+        scanner.close();
+        return line;
     }
 }
