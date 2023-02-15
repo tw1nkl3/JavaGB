13a0f1b3eb73267a950e0e7c32a7e6ad24c50241
Ivan <grabovivan@gmailcom>
2023-02-15 11:50:23 +0300

    readFile method added

--- hw2/num1.java
+++ hw2/num1.java
@@ -1,7 +1,25 @@
 package hw2;
 
+import java.io.*;
+import java.util.*;
+
 public class num1 {
     public static void main(String[] args) {
-        
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void readFile() throws FileNotFoundException{
+        File txt = new File("hw2/input.txt");
+        Scanner scan = new Scanner(txt);
+        ArrayList<String> data = new ArrayList<String>() ;
+        while(scan.hasNextLine()){
+            data.add(scan.nextLine());
+        }
+        System.out.println(data);
+        String[] simpleArray = data.toArray(new String[]{});
     }
 }
